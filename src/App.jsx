c780bab2a861a93d697788c3bd4bc72f45b2719f
shahadThayyil <shahadthayyil1@gmail.com>
2025-09-19 14:10:29 +0530

    adding loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Lenis from "@studio-freight/lenis";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import SplashCursor from "./components/animations/SplashCursor";
 import Projects from "./pages/Projects";
@@ -15,26 +15,14 @@ import ScrollToTop from "./components/ScrollToTop";
 import ProjectDetail from "./pages/ProjectDetail";
 import UseSpotlightEffect from "./components/animations/useSpotlightEffect";
 import SpotlightCursor from "./components/SpotlightCursor";
+import { Grid } from "ldrs/react";
+// import "ldrs/react/Grid.css";
 
 function App() {
-  // useEffect(() => {
-  //   const lenis = new Lenis({
-  //     duration: 1.5,
-  //     easing: (t) => 1 - Math.pow(1 - t, 3),
-  //     smooth: true,
-  //   });
+  const [loading, setLoading] = useState(true);
 
-  //   function raf(time) {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(raf);
-  //   }
-  //   requestAnimationFrame(raf);
-
-  //   return () => {
-  //     lenis.destroy(); // Cleanup when component unmounts
-  //   };
-  // }, []);
- useEffect(() => {
+  // Lenis smooth scroll setup
+  useEffect(() => {
     const lenis = new Lenis({
       duration: 1.5,
       easing: (t) => 1 - Math.pow(1 - t, 3),
@@ -45,23 +33,39 @@ function App() {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
 
-    // Cleanup only Lenis instance
     return () => {
       lenis.destroy();
     };
   }, []);
 
+  // Loader control
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+    return () => window.removeEventListener("load", handleLoad);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen flex items-center justify-center bg-white">
+        <Grid size="60" speed="1.5" color="black" />
+      </div>
+    );
+  }
+
   return (
     <Router>
-     
       <div className="main">
-         <ScrollToTop />
+        <ScrollToTop />
         {/* <SplashCursor /> */}
         {/* <UseSpotlightEffect /> */}
         {/* <SpotlightCursor /> */}
+
         {/* Navbar always visible */}
         <div className="absolute top-0 left-0 w-full z-20">
           <CardNav
